a550ebc87e4541cbdfe491b2ab127a6b24fb4a4c
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-08-28 18:53:04 -0500

    fixed conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   
 
   get "/" => "pages#index"
-<<<<<<< HEAD
   get "/shoes" => "pages#index"
-=======
   get "/show_stuff" => "pages#show"
->>>>>>> 797a7f79159e2c62954733155905786ecd8c3bc3
+
 end
