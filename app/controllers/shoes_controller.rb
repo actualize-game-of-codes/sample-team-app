d4980e326ab5b5a920d378f7a298944605fa3f37
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-08-28 18:32:21 -0500

    Created route, controller and view Shoes

---
+++ app/controllers/shoes_controller.rb
@@ -0,0 +1,5 @@
+class ShoesController < ApplicationController
+  def index
+    render "shoes.html.erb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  
+
   get "/" => "pages#index"
+  get "/shoes" => "pages#index"
 end
